971ba346fc3478e56c90705e78a16ca552e70d97
rzounlom <rzounlom@gmail.com>
2024-12-14 10:33:47 -0500

    scaffold folder structure for pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
+
+import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'HomeAway Draft',
+  title: 'HomeAway',
   description: 'Feel at home, away from home.',
 };
 
