316e1fa70f0850ec695cfd81cbd30fc7473bb185
rzounlom <rzounlom@gmail.com>
2025-01-06 17:16:12 -0500

    add user image to navbar

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,20 @@
-import { LuUser } from 'react-icons/lu';
+import Image from "next/image";
+import { LuUser } from "react-icons/lu";
+import { fetchProfileImage } from "@/utils/actions";
 
-export default function UserIcon() {
-  return <LuUser className='w-6 h-6 bg-primary rounded-full text-white' />;
+export default async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+
+  if (profileImage)
+    return (
+      <Image
+        src={profileImage}
+        className="w-6 h-6 rounded-full object-cover"
+        height={24}
+        width={24}
+        alt="profile image"
+      />
+    );
+
+  return <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,18 +1,56 @@
 "use server";
 
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
+
+import db from "./db";
 import { profileSchema } from "./schemas";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
+    const user = await currentUser();
+    if (!user) throw new Error("Please login to create a profile");
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
-    return { message: "Profile Created" };
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
-    console.log(error);
-    return { message: "there was an error..." };
+    console.error(error);
+    return {
+      message: error instanceof Error ? error.message : "An error occurred",
+    };
   }
+  redirect("/");
+};
+
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      profileImage: true,
+    },
+  });
+  return profile?.profileImage;
 };
