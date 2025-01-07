d9f3548713b31e17bf493863b4480f526dfd5061
rzounlom <rzounlom@gmail.com>
2025-01-07 07:06:48 -0500

    complete create property page

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -9,14 +9,13 @@ import PriceInput from "@/components/form/PriceInput";
 import { SubmitButton } from "@/components/form/Buttons";
 import TextAreaInput from "@/components/form/TextAreaInput";
 import { createPropertyAction } from "@/utils/actions";
-
-function CreateProperty() {
+function CreatePropertyPage() {
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">
         create property
       </h1>
-      <div className="border p-8 rounded-md">
+      <div className="border p-8 rounded">
         <h3 className="text-lg mb-4 font-medium">General Info</h3>
         <FormContainer action={createPropertyAction}>
           <div className="grid md:grid-cols-2 gap-8 mb-4">
@@ -28,19 +27,19 @@ function CreateProperty() {
             />
             <FormInput
               name="tagline"
-              type="text "
+              type="text"
               label="Tagline (30 limit)"
-              defaultValue="Dream Getaway Awaits You Here!"
+              defaultValue="Dream Getaway Awaits You Here"
             />
             {/* price */}
-            <PriceInput defaultValue={100} />
+            <PriceInput />
             {/* categories */}
             <CategoriesInput />
           </div>
           {/* text area / description */}
           <TextAreaInput
             name="description"
-            labelText="Description (10 - 1000 Words)"
+            labelText="Description (10 - 1000 words)"
           />
           <div className="grid sm:grid-cols-2 gap-8 mt-4">
             <CountriesInput />
@@ -61,4 +60,4 @@ function CreateProperty() {
     </section>
   );
 }
-export default CreateProperty;
+export default CreatePropertyPage;
