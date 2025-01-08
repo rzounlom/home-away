54b8242846aaa7bb9f178c1a3027db5788573c23
rzounlom <rzounlom@gmail.com>
2025-01-08 06:19:22 -0500

    add cards skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import CategoriesList from "@/components/home/CategoriesList";
+import LoadingCards from "@/components/card/LoadingCards";
 import PropertiesContainer from "@/components/home/PropertiesContainer";
+import { Suspense } from "react";
 
 function HomePage({
   searchParams,
@@ -13,10 +15,12 @@ function HomePage({
         category={searchParams.category}
         search={searchParams.search}
       />
-      <PropertiesContainer
-        category={searchParams.category}
-        search={searchParams.search}
-      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams.category}
+          search={searchParams.search}
+        />
+      </Suspense>
     </section>
   );
 }
