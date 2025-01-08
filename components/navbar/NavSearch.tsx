33ccafee05898f1163ef91713c505730a5c445d1
rzounlom <rzounlom@gmail.com>
2025-01-08 06:30:02 -0500

    update nav search

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,12 +1,45 @@
-import { Input } from '../ui/input';
+"use client";
 
-export default function NavSearch() {
+import { useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+import { Input } from "../ui/input";
+import { useDebouncedCallback } from "use-debounce";
+
+function NavSearch() {
+  const searchParams = useSearchParams();
+  const getSearch = searchParams.get("search");
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [search, setSearch] = useState(getSearch?.toString() || "");
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [getSearch, searchParams]);
+
   return (
     <Input
-      type='search'
-      placeholder='find a property...'
-      className='max-w-xs dark:bg-muted '
+      type="search"
+      placeholder="find a property..."
+      className="max-w-xs dark:bg-muted "
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
     />
   );
 }
-NavSearch;
+export default NavSearch;
