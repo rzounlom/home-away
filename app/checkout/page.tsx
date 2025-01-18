87b58a16780a75f2e0d8cfe98c6b661b34f0fc53
rzounlom <rzounlom@gmail.com>
2025-01-18 15:00:10 -0500

    add stripe routes and functionality

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,39 @@
-function CheckoutPage() {
-    return <h1 className='text-3xl'>CheckoutPage</h1>;
-  }
-  export default CheckoutPage;
+"use client";
+
+import {
+  EmbeddedCheckout,
+  EmbeddedCheckoutProvider,
+} from "@stripe/react-stripe-js";
+import React, { useCallback } from "react";
+
+import axios from "axios";
+import { loadStripe } from "@stripe/stripe-js";
+import { useSearchParams } from "next/navigation";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
+
+export default function CheckoutPage() {
+  const searchParams = useSearchParams();
+
+  const bookingId = searchParams.get("bookingId");
+
+  const fetchClientSecret = useCallback(async () => {
+    // Create a Checkout Session
+    const response = await axios.post("/api/payment", {
+      bookingId: bookingId,
+    });
+    return response.data.clientSecret;
+  }, []);
+
+  const options = { fetchClientSecret };
+
+  return (
+    <div id="checkout">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
+}
